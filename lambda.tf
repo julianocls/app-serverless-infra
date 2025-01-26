01887393f50ea262874b1f9d279dc78b199fdc61
Juliano C L Santos <foxports@hotmail.com>
2025-01-25 23:49:44 -0300

    Criando lambda - passo 2

--- lambda.tf
+++ lambda.tf
@@ -15,3 +15,28 @@ resource "aws_lambda_function" "s3" {
 
   tags = local.common_tags
 }
+
+data "archive_file" "dynamo" {
+  type        = "zip"
+  source_file = "${local.lambdas_path}/dynamo/index.js"
+  output_path = "files/dynamo-artefact.zip"
+}
+
+resource "aws_lambda_function" "dynamo" {
+  function_name = "dynamo"
+  handler       = "index.handler"
+  role          = aws_iam_role.dynamo.arn
+  runtime       = "nodejs18.x"
+
+  filename         = data.archive_file.dynamo.output_path
+  source_code_hash = data.archive_file.dynamo.output_base64sha256
+
+  timeout     = 30
+  memory_size = 128
+
+  environment {
+    variables = {
+      TABLE = aws_dynamodb_table.this.name
+    }
+  }
+}
