5f9eb2b92aab8d557ccee7ba9fb554f5342e2cf8
Juliano C L Santos <foxports@hotmail.com>
2025-01-26 16:50:39 -0300

    Criando api-gateway

--- lambda.tf
+++ lambda.tf
@@ -13,6 +13,12 @@ resource "aws_lambda_function" "s3" {
   filename         = data.archive_file.s3.output_path
   source_code_hash = data.archive_file.s3.output_base64sha256
 
+  environment {
+    variables = {
+      TOPIC_ARN = aws_sns_topic.this.arn
+    }
+  }
+
   tags = local.common_tags
 }
 
@@ -47,4 +53,20 @@ resource "aws_lambda_function" "dynamo" {
       TABLE = aws_dynamodb_table.this.name
     }
   }
+}
+
+resource "aws_lambda_permission" "dynamo" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.dynamo.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:${var.aws_region}:${var.aws_account_id}:*/*"
+}
+
+resource "aws_lambda_permission" "sns" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.dynamo.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.this.arn
 }
--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,18 @@ output "cognito_url" {
   value = "https://${aws_cognito_user_pool_domain.this.domain}.auth.${var.aws_region}.amazoncognito.com"
 }
 
+output "lambda_s3_url" {
+  value = aws_lambda_function.s3.invoke_arn
+}
+
+output "lambda_dynamo_url" {
+  value = aws_lambda_function.dynamo.invoke_arn
+}
+
+output "api_url" {
+  value = aws_api_gateway_deployment.this.invoke_url
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.todo.bucket
+}
